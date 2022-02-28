28662e8a758c5375cdde011a2227fe18050df698
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-02-28 00:04:17 -0300

    Fazendo classes, funções, atributos

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-function pagar(){
-    let tquantidade = document.getElementById('quantidade')
-    let tvalor = document.getElementById('valor')
-    let quantidade = Number(tquantidade.value)
-    let valor = Number(tvalor.value)
-    let total= quantidade * valor 
-    total.innerHTML = `O valor a ser pago é de R$ ${total}`
-    
+class Produtos {
 
+
+
+    constructor(){
+        this.id = 0
+        this.nomeProduto = ''
+        this.valor = 0
+
+    }
+
+    cadastrar(){
+        alert('add produtos ?')
+    }
+
+    excluir(){
+        alert('item deletado')
+    }
 }
+
+let produtos = new Produtos()
 
