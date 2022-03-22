328ab134c836483606ac7b86b10d833770b1201e
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-03-21 23:55:49 -0300

    iniciando inclusão dinamica de produtos e preços

--- tela.js
+++ tela.js
@@ -1,7 +1,47 @@
+let arrayProdutos = []
+function chama (){
+    adicionar()
+    compra()
+    listaCompra()
+}
 function compra (){
     let qtd = document.getElementById('quantidade').value 
     let preco = document.getElementById('preco').value 
     let tpreço = qtd * preco
     alert(tpreço)
 }
+function adicionar (produto){
+    produto.preço = parseFloat(produto.preço)
+    this.arrayProdutos.push(produto)
+    this.id++
+}
+
+function listaCompra(){
+    let corpo = document.getElementById('corpo')
+    corpo.innerText = ''
+        for(let i = 0; i < arrayProdutos.length; i++){
+            let tr = corpo.insertRow()
+            let td_id = tr.insertCell()
+            let td_produto = tr.insertCell()
+            let td_preco = tr.insertCell()
+            let td_acoes = tr.insertCell()
+            td_id.innerText = this.arrayProdutos[i].id
+            td_produto.innerText = this.arrayProdutos[i].nameProduto
+            td_preco.innerText = this.arrayProdutos[i].preço
+            
+            let imgEdit = document.createElement('img')
+            imgEdit.src = 'img/editar-arquivo.png'
+            imgEdit.setAttribute('onclick', 'produto.editar('+ JSON.stringify(this.arrayProdutos[i]) + ')')
+
+            let imgExcluir = document.createElement('img')
+            imgExcluir.src = 'img/excluir.png'
+            imgExcluir.setAttribute('onclick','produto.deletar('+ this.arrayProdutos[i].id +')')
+
+            td_acoes.appendChild(imgEdit)
+            td_acoes.appendChild(imgExcluir)
+        }
+    }
+
+
+
 
